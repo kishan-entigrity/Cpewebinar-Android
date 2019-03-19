b5f30cfe16f105bfd4bb01ffff21d6b220c6523e
chirag <chirag@entigrity.com>
2019-03-19 16:36:42 +0530

    19_03_2019_My_Credit_Screen

---
+++ app/src/main/java/com/entigrity/adapter/MyCreditAdapter.java
@@ -0,0 +1,56 @@
+package com.entigrity.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.entigrity.R;
+import com.entigrity.webservice.APIService;
+import com.entigrity.webservice.ApiUtils;
+
+public class MyCreditAdapter extends RecyclerView.Adapter<MyCreditAdapter.ViewHolder> {
+
+    private Context mContext;
+    private APIService mAPIService;
+    LayoutInflater mInflater;
+
+    public MyCreditAdapter(Context mContext) {
+        this.mContext = mContext;
+        mAPIService = ApiUtils.getAPIService();
+        mInflater = (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    @NonNull
+    @Override
+    public MyCreditAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_mycredit, viewGroup, false);
+        return new MyCreditAdapter.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int position) {
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        private ViewHolder(View itemView) {
+            super(itemView);
+
+
+        }
+    }
+}
--- app/src/main/java/com/entigrity/fragments/MyCreditsFragment.java
+++ app/src/main/java/com/entigrity/fragments/MyCreditsFragment.java
@@ -2,17 +2,19 @@ package com.entigrity.fragments;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.entigrity.R;
+import com.entigrity.adapter.MyCreditAdapter;
 import com.entigrity.databinding.FragmentMycreditBinding;
+import com.entigrity.view.SimpleDividerItemDecoration;
 
 public class MyCreditsFragment extends Fragment {
 
@@ -20,12 +22,27 @@ public class MyCreditsFragment extends Fragment {
     public Context context;
     FragmentMycreditBinding binding;
     private static final String TAG = MyCreditsFragment.class.getName();
+    MyCreditAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mycredit, null, false);
         context = getActivity();
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+        binding.recyclerviewMycredit.setLayoutManager(layoutManager);
+
+        binding.recyclerviewMycredit.addItemDecoration(new SimpleDividerItemDecoration(context));
+
+
+        adapter = new MyCreditAdapter(getActivity());
+
+
+        if (adapter != null) {
+            binding.recyclerviewMycredit.setAdapter(adapter);
+        }
 
 
         return view = binding.getRoot();
