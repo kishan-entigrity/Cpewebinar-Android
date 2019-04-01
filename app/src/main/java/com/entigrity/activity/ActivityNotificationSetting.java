d79dae90d93a56d2368f20efea832f6f26ce777f
chirag <chirag@entigrity.com>
2019-04-01 22:52:12 +0530

    01_04_2019_notification_screen_completed

--- app/src/main/java/com/entigrity/activity/ActivityNotificationSetting.java
+++ app/src/main/java/com/entigrity/activity/ActivityNotificationSetting.java
@@ -14,7 +14,10 @@ import com.entigrity.databinding.ActivityNotificationSettingBinding;
 public class ActivityNotificationSetting extends AppCompatActivity {
 
     ActivityNotificationSettingBinding binding;
-    public boolean checkexpand = true;
+    public boolean checkexpand_pushnoti_webinar = true;
+    public boolean checkexpand_pushnoti_pending_action = true;
+    public boolean checkexpand_text_webinar = true;
+    public boolean checkexpand_text_pending_action = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,13 +29,13 @@ public class ActivityNotificationSetting extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (checkexpand) {
-                    checkexpand = false;
+                if (checkexpand_pushnoti_webinar) {
+                    checkexpand_pushnoti_webinar = false;
                     collapse(binding.relRegisterNotification, 500, 0);
                     collapse(binding.relReminderNotification, 500, 0);
 
                 } else {
-                    checkexpand = true;
+                    checkexpand_pushnoti_webinar = true;
                     expand(binding.relRegisterNotification, 500, 50);
                     expand(binding.relReminderNotification, 500, 50);
                 }
@@ -40,6 +43,59 @@ public class ActivityNotificationSetting extends AppCompatActivity {
 
             }
         });
+
+        binding.relPendingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkexpand_pushnoti_pending_action) {
+                    checkexpand_pushnoti_pending_action = false;
+                    collapse(binding.relPushQuizPending, 500, 0);
+                    collapse(binding.relPushPendingEvolution, 500, 0);
+
+                } else {
+                    checkexpand_pushnoti_pending_action = true;
+                    expand(binding.relPushQuizPending, 500, 50);
+                    expand(binding.relPushPendingEvolution, 500, 50);
+                }
+
+            }
+        });
+
+
+        binding.relWebinarText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkexpand_text_webinar) {
+                    checkexpand_text_webinar = false;
+                    collapse(binding.relTextRegister, 500, 0);
+                    collapse(binding.relTextReminder, 500, 0);
+
+                } else {
+                    checkexpand_text_webinar = true;
+                    expand(binding.relTextRegister, 500, 50);
+                    expand(binding.relTextReminder, 500, 50);
+                }
+
+            }
+        });
+
+        binding.relTextPendingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkexpand_text_pending_action) {
+                    checkexpand_text_pending_action = false;
+                    collapse(binding.quizTextPending, 500, 0);
+                    collapse(binding.relTextPendingEvolution, 500, 0);
+
+                } else {
+                    checkexpand_text_pending_action = true;
+                    expand(binding.quizTextPending, 500, 50);
+                    expand(binding.relTextPendingEvolution, 500, 50);
+                }
+
+            }
+        });
+
 
         binding.ivback.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,58 +107,6 @@ public class ActivityNotificationSetting extends AppCompatActivity {
 
     }
 
-
-  /*  public static void expand(final View v) {
-        v.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        final int targetHeight = v.getMeasuredHeight();
-
-        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
-        v.getLayoutParams().height = 1;
-        v.setVisibility(View.VISIBLE);
-        Animation a = new Animation() {
-            @Override
-            protected void applyTransformation(float interpolatedTime, Transformation t) {
-                v.getLayoutParams().height = interpolatedTime == 1
-                        ? ViewGroup.LayoutParams.WRAP_CONTENT
-                        : (int) (targetHeight * interpolatedTime);
-                v.requestLayout();
-            }
-
-            @Override
-            public boolean willChangeBounds() {
-                return true;
-            }
-        };
-
-        // 1dp/ms
-        a.setDuration((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density));
-        v.startAnimation(a);
-    }
-
-    public static void collapse(final View v) {
-        final int initialHeight = v.getMeasuredHeight();
-
-        Animation a = new Animation() {
-            @Override
-            protected void applyTransformation(float interpolatedTime, Transformation t) {
-                if (interpolatedTime == 1) {
-                    v.setVisibility(View.GONE);
-                } else {
-                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
-                    v.requestLayout();
-                }
-            }
-
-            @Override
-            public boolean willChangeBounds() {
-                return true;
-            }
-        };
-
-        // 1dp/ms
-        a.setDuration((int) (initialHeight / v.getContext().getResources().getDisplayMetrics().density));
-        v.startAnimation(a);
-    }*/
 
     public static void expand(final View v, int duration, int targetHeight) {
 
